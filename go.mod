7715ac6b7fc1d18ad1dd41de5a2e65224b97e004
shikhar0507 <kap.shikhar@gmail.com>
2021-03-08 20:59:43 +0530

    login completed

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module auth
 go 1.16
 
 require (
-	github.com/jackc/pgx v3.6.2+incompatible // indirect
-	github.com/jackc/pgx/v4 v4.10.1 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
+	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
+	github.com/jackc/pgx/v4 v4.10.1
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
 	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- server.go
+++ server.go
@@ -1,31 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
-	"github.com/jackc/pgx/v4/pgxpool"
-	"os"
-	"context"
 	"auth/pageLoader"
 	"auth/pageStruct"
+	"auth/requestDecoder"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+	"os"
 )
 
 var db *pgxpool.Pool
 
-
 func main() {
+
+
+
 	dbpool, err := pgxpool.Connect(context.Background(),os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
 	}
 	db = dbpool
 	defer db.Close()
+
 	http.HandleFunc("/",handleSessionState)
 	http.HandleFunc("/login",handleSessionState)
-	log.Fatal(http.ListenAndServe(":8080",nil))
+
+	http.HandleFunc("/signup",handleSignup)
+	http.HandleFunc("/favicon.ico", faviconHandler)
+
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080",nil))
 }
 
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "public/favicon.ico")
+}
 
 func handleSessionState (w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.Path)
@@ -93,52 +106,117 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	pageLoader.LoadPage(w,"index",homePageData)
 }
 
+// 1) parse the request and check if format matches
+// 2) check if user exists
+// 3) if exists then reject else create new user and set cookie
+
+func handleSignup(w http.ResponseWriter,r *http.Request) {
+
+	type AuthBody struct {
+		Username string
+		Psswd string
+	}
+	var signupBody AuthBody
+	//var result requestDecoder.Result
+
+	result := requestDecoder.Decode(w,r,&signupBody)
+	if signupBody.Username == "" {
+		http.Error(w,"Username cannot be empty",http.StatusBadRequest)
+		return
+	}
+
+	if signupBody.Psswd == "" {
+		http.Error(w,"Password cannot be empty",http.StatusBadRequest)
+		return
+	}
+	fmt.Println("check for  user")
+	if userExists(signupBody.Username,signupBody.Psswd) {
+		fmt.Println("user exist")
+		http.Error(w,"Account already exist!. Please login",http.StatusConflict)
+		return
+	}
+	fmt.Println(result)
+	if result.Status == 200 {
+		// if request is successfully parsed
+		err := createUser(signupBody.Username,signupBody.Psswd)
+		if err != nil {
+			http.Error(w,"Error creating success response",http.StatusInternalServerError)
+			return
+		}
+		successResponse, marshalErr := json.Marshal(result)
+		if marshalErr != nil {
+			http.Error(w,"Error creating success response",http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprint(w,string(successResponse))
+		return
+	}
+	fmt.Println("error",result)
+	http.Error(w,result.Message,result.Status)
+}
+
+
+func getSessionCookie (r *http.Request) string {
+	coo,err := r.Cookie("session")
+	if err != nil {
+		return ""
+	}
+	return coo.Value
+}
+
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Login page")
 
-	username := r.FormValue("username")
-	psswd := r.FormValue("password")
-	if username == "" && psswd == "" {
-		loginData := pageStruct.PageData{Title: "Login",Login: true}
-		pageLoader.LoadPage(w,"login",loginData)
 
-		return
-	}
-	if userExists(username,psswd) {
-		fmt.Println("user exist")
-		http.Redirect(w,r,"/",302)
-		return
-	}
-	_,err := createUser(username,psswd)
-	if err != nil {
-		fmt.Fprintf(w,"Failed to create account")
-	}
 	loginData := pageStruct.PageData{Title: "Login",Login: true}
 	pageLoader.LoadPage(w,"login",loginData)
 
-	fmt.Print("Failed to create user")
-	cookie := http.Cookie{Name:"session",Value:username}
-	http.SetCookie(w,&cookie)
-	http.Redirect(w,r,"/",302)
+	//fmt.Print("Failed to create user")
+	//cookie := http.Cookie{Name:"session",Value:username}
+	//http.SetCookie(w,&cookie)
+	//http.Redirect(w,r,"/",302)
 }
 
-func createUser (username string,psswd string) (bool,error) {
-	_,err := db.Exec(context.Background(), "insert into auth values($1,$2)",username,psswd)
+
+func generatePsswdHash (psswd string) ([]byte,error) {
+
+	bytePsswd := []byte(psswd)
+	hash,err := bcrypt.GenerateFromPassword(bytePsswd,bcrypt.DefaultCost)
 	if err != nil {
 		fmt.Println(err)
-		return false ,err
+		return nil,err
 	}
-	return true,nil
+	return hash,nil
+
+}
+
+func createUser (username string,psswd string) error {
+	hash, err := generatePsswdHash(psswd)
+	if err != nil {
+		return err
+	}
+	_,err = db.Exec(context.Background(), "insert into auth values($1,$2)",username,hash)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 
 
 }
 
-func userExists (username string, password string) bool {
+func userExists (username string,psswd string) bool {
 	var savedUsername string;
-	err := db.QueryRow(context.Background(),"select username from auth where username=$1",username).Scan(&savedUsername)
-	fmt.Println("query Err",err)
-	fmt.Println("found",savedUsername)
+	hash , err := generatePsswdHash(psswd)
+	fmt.Println("hash",hash)
 	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	err = db.QueryRow(context.Background(),"select * from auth where username=$1 and hash=$2",username,hash).Scan(&savedUsername)
+	fmt.Println(savedUsername)
+	if err != nil {
+		fmt.Println(err)
 		return false
 	}
 	if savedUsername != "" {
@@ -146,4 +224,5 @@ func userExists (username string, password string) bool {
 	}
 	return false
 }
+
 
